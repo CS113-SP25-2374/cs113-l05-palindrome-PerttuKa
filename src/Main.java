ea970d652905066376912291b623fe3dc4e62dc9
PerttuKa <pkaislasaari@gmail.com>
2025-03-25 13:41:28 -0700

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String string = "TACOCAT";
+        Palindrome test = new Palindrome();
+
+        boolean isPalindrome = test.isPalindrome(string);
+
+        if(isPalindrome) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+
     }
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -12,6 +12,33 @@ public class Palindrome {
      * @return true if they are the same
      */
     public boolean isPalindrome(String s) {
+
+        ArrayListStack<Character> first = new ArrayListStack<>();
+        ArrayListStack<Character> second = new ArrayListStack<>();
+        ArrayListStack<Character> flipped = new ArrayListStack<>();
+
+
+        int length =  s.length();
+
+
+        for (int i = 0; i < length/2; i++){
+            first.push(s.charAt(i));
+        }
+
+        //Used AI to help fix
+        for (int i = length/2 + length%2; i < length; i++){
+            second.push(s.charAt(i));
+        }
+
+        while(!second.empty()){
+            flipped.push(second.pop());
+        }
+
+        if(!first.empty()){
+            return first.pop().equals(flipped.pop());
+        }
+
         return true;
+
     }
 }
